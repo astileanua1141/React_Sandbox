7577fe5fdd039eeac672e86e557144cb1dfadfab
Alexandra Astileanu <Alexandra.Astileanu@gmail.com>
2022-09-29 14:39:11 +0300

    Add a moderation service that filters comment contents

--- 1-small-react-blog/query/index.js
+++ 1-small-react-blog/query/index.js
@@ -26,11 +26,22 @@ app.post("/events", (req, res) => {
     };
   }
   if (type === "CommentCreated") {
-    const { id, content, postId } = data
+    const { id, content, postId, status } = data
     const post = posts[postId]
     post.comments.push({
-        id, content
+        id, content, status
     })
+  }
+  if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data
+    
+    const post = posts[postId]
+    const comment = post.comments.find(comment => {
+      return comment.id === id
+    })
+
+    comment.content = content;
+    comment.status = status;
   }
   console.log(posts)
   res.send({});
